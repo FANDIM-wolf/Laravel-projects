7a73d89a8445c43a55a564e15d7349842e513e06
Mikhail Shtainer <63235817+FANDIM-wolf@users.noreply.github.com>
2021-04-13 22:48:18 +0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
+use App\Models\ReviewModel;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,6 +22,12 @@ Route::get('/', 'MainController@get_home_page');
 
 Route::get('/review', 'MainController@reviews')->name('review');
 Route::get('/review/check', 'MainController@review_check');
+//Get post
+Route::get('reviews/{id}',function ($id){
+    $objects = new ReviewModel;
+    $object = $objects::where('id','=',$id)->first();
+    return view('post' ,['object'=>$object]);
+});
 //Route::get('/user{id}/{name}', function ($id ,$name) {
   //  return view('about');
 //});
